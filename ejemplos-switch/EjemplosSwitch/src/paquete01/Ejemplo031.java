3def33412113fa4eb139992c60de29256865dcbc
Jonathan <jdguayllas@utpl.edu.ec>
2023-11-15 16:56:48 -0500

    tercer commit

---
+++ ejemplos-switch/EjemplosSwitch/src/paquete01/Ejemplo031.java
@@ -0,0 +1,73 @@
+/*
+
+ANA DIAZ con edad 19, es estudiante de UTPL
+
+luis vera con edad 19, es estudiante de UNL
+
+ */
+package paquete01;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo031 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        System.out.println("Ingrese su nombre");
+        String nombre = entrada.nextLine();
+
+        System.out.println("Ingrese su apellido");
+        String apellido = entrada.nextLine();
+
+        System.out.println("Ingrese su edad");
+        int edad = entrada.nextInt();
+
+        entrada.nextLine();
+
+        System.out.println("Ingrese su universidad");
+        String universidad = entrada.nextLine();
+
+        // nombre = "atacames"
+        char valor = nombre.charAt(0); // obtener el primer caracter de una
+        // cadena
+        //  L   o   j   a
+        //  0   1   2   3
+        switch (valor) {
+            case 'a':
+            case 'A':
+            case 'e':
+            case 'E':
+            case 'i':
+            case 'I':
+            case 'o':
+            case 'O':
+            case 'u':
+            case 'U':
+                System.out.printf("%s %s con edad %d, es estudiante de "
+                        + "%s\n", nombre.toUpperCase(),
+                        apellido.toUpperCase(), edad,
+                        universidad.toUpperCase());
+                break;
+
+            default:
+                System.out.printf("%s %s con edad %d, es estudiante de "
+                        + "%s\n", nombre.toLowerCase(),
+                        apellido.toLowerCase(), edad,
+                        universidad.toUpperCase());
+                break;
+
+        }
+
+    }
+}
